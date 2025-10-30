16e3e1e0bfc8866ba242233b6d2c8dc6b7b688bd
Tiansheng <otterlover24@gmail.com>
2025-10-30 20:59:10 +0800

    Adds intermediate step.

--- react-langchain/main.py
+++ react-langchain/main.py
@@ -1,11 +1,23 @@
 from dotenv import load_dotenv
-from langchain.tools import tool
+from langchain.tools import tool, BaseTool
 from langchain_core.tools import render_text_description
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_core.agents import AgentAction, AgentFinish
 from langchain_core.output_parsers import BaseOutputParser
+from typing import Union, List
 import re
+
+
+def format_log_to_str(intermediate_steps: List[tuple]) -> str:
+    """Format intermediate steps to string."""
+    if not intermediate_steps:
+        return ""
+    thoughts = ""
+    for action, observation in intermediate_steps:
+        thoughts += f"{action.log}\nObservation: {observation}\nThought: "
+    return thoughts
+
 
 load_dotenv()
 
@@ -37,15 +49,23 @@ class ReActSingleInputOutputParser(BaseOutputParser):
 @tool
 def get_text_length(text: str) -> int:
     """Returns the length of the input text in terms of number of characters."""
+    print(f"enter get_text_length  with {text=}")
     text = text.strip()
     return len(text)
+
+
+def find_tool_by_name(tools: List[BaseTool], tool_name: str) -> BaseTool:
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+    raise ValueError(f"Tool with name {tool_name} not found.")
 
 
 if __name__ == "__main__":
     # Your main application logic here
     # print("Hello, ReAct LangChain!")
     # print(f"Length of sample text: {get_text_length.invoke("Dog")}")
-    tools = [get_text_length]
+    tools: List[BaseTool] = [get_text_length]
 
     template = """
 Answer the following questions as best you can. You have access to the following tools:
@@ -66,7 +86,7 @@ Final Answer: the final answer to the original input question
 Begin!
 
 Question: {input}
-Thought:
+Thought: {agent_scratchpad}
     """
 
     prompt = PromptTemplate.from_template(template=template).partial(
@@ -74,11 +94,44 @@ Thought:
         tool_names=", ".join([tool.name for tool in tools]),
     )
 
-    llm = ChatOpenAI(temperature=0).bind(stop=["\nObservation:", "Observation:"])
+    # llm = ChatOpenAI(temperature=0).bind(stop=["\nObservation:", "Observation:"])
+    llm = ChatOpenAI(temperature=0)
+
+    intermediate_steps = []
 
     agent = (
-        {"input": lambda x: x["input"]} | prompt | llm | ReActSingleInputOutputParser()
+        {
+            "input": lambda x: x.get("input", ""),
+            "agent_scratchpad": lambda x: format_log_to_str(
+                x.get("agent_scratchpad", [])
+            ),
+        }
+        | prompt
+        | llm
+        | ReActSingleInputOutputParser()
     )
 
-    res = agent.invoke({"input": "What is the length of the word 'DOG'?"})
-    print(res)
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+        input={"input": "What is the length of the word DOG?"},
+        agent_scratchpad=intermediate_steps,
+    )
+    print(agent_step)
+
+    if isinstance(agent_step, AgentAction):
+        tool_name = agent_step.tool
+        tool_to_use = find_tool_by_name(tools, tool_name)
+        tool_input = agent_step.tool_input
+
+        observation = tool_to_use.invoke(tool_input)
+        print(f"Observation: {observation}")
+        intermediate_steps.append((agent_step, observation))
+
+    print(
+        f"Agent scratchpad so far:\n{format_log_to_str(intermediate_steps)}\n END OF SCRATCHPAD"
+    )
+
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+        input={"input": "What is the length of the word DOG?"},
+        agent_scratchpad=intermediate_steps,
+    )
+    print(agent_step)
