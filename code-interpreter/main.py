86ec7f2b3b2890d49168cf2727f0367db91e2cd7
Tiansheng <otterlover24@gmail.com>
2025-11-03 15:57:47 +0800

    Adds overall router agent.

--- code-interpreter/main.py
+++ code-interpreter/main.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv
+from typing import Any
 from langchain import hub
 from langchain_openai import ChatOpenAI
-from langchain.agents import create_react_agent, AgentExecutor
+from langchain.agents import create_react_agent, AgentExecutor, Tool
 from langchain_experimental.agents.agent_toolkits import create_csv_agent
 from langchain_experimental.tools import PythonREPLTool
 
@@ -48,6 +49,51 @@ def main():
         input={
             "input": "HWhich writer wrote the most episodes? How many episodes did he write??"
         }
+    )
+
+    ############# Router Grand Agent ############################
+    def python_agent_executor_wrapper(original_prompt: str) -> dict[str, Any]:
+        return agent_executor.invoke({"input": original_prompt})
+
+    tools = [
+        Tool(
+            name="Python Agent",
+            func=python_agent_executor_wrapper,
+            description="""Useful when you need to transform natural language to Python and execute the code,
+            returning the results of the code execution.
+            DOES NOT ACCEPT CODE AS INPUT.""",
+        ),
+        Tool(
+            name="CSV Agent",
+            func=csv_agent.invoke,
+            description="""Useful when you need to answer questions about the data in episode_info.csv.
+            Takes an input the entire question and returns the answer after runing Pandas calculations""",
+        ),
+    ]
+
+    prompt = base_prompt.partial(instructions="")
+    grand_agent = create_react_agent(
+        prompt=prompt,
+        llm=ChatOpenAI(temperature=0, model="gpt-4-turbo"),
+        tools=tools,
+    )
+    grand_agent_executor = AgentExecutor(agent=grand_agent, tools=tools, verbose=True)
+
+    print(
+        grand_agent_executor.invoke(
+            {
+                "input": "which season has the most episodes?",
+            }
+        )
+    )
+
+    print(
+        grand_agent_executor.invoke(
+            input={
+                "input": """Generate and save in current working directory 15 QR codes that point to
+            www.udemy.com/course/langchain. You have the qrcode package installed already."""
+            }
+        )
     )
 
 
